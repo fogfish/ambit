5709407cff4852f8664f61ee29903ca189648ce6
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2016-10-25 22:45:29 +0300

    use transient strategy for bride process

--- src/ambit_actor_bridge_sup.erl
+++ src/ambit_actor_bridge_sup.erl
@@ -26,10 +26,9 @@
 ]).
 
 %%
--define(CHILD(I),                  {service,                I, permanent, infinity, worker, dynamic}).
--define(CHILD(Type, I),            {I,  {I, start_link,   []}, permanent, infinity,   Type, dynamic}).
--define(CHILD(Type, I, Args),      {I,  {I, start_link, Args}, permanent, infinity,   Type, dynamic}).
--define(CHILD(Type, ID, I, Args),  {ID, {I, start_link, Args}, permanent, infinity,   Type, dynamic}).
+-define(CHILD(Type, I),            {I,  {I, start_link,   []}, transient, infinity,   Type, dynamic}).
+-define(CHILD(Type, I, Args),      {I,  {I, start_link, Args}, transient, infinity,   Type, dynamic}).
+-define(CHILD(Type, ID, I, Args),  {ID, {I, start_link, Args}, transient, infinity,   Type, dynamic}).
 
 %%-----------------------------------------------------------------------------
 %%
